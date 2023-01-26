f9c08df7b44f4260207543aba9768a7d2397501b
Enoth <58894013+nikitairl@users.noreply.github.com>
2023-01-26 17:36:15 +0300

    set

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -30,7 +30,7 @@ INSTALLED_APPS = [
     'djoser',
     'colorfield',
     'django_filters',
-    'foodgram'
+    'foodgram',
 ]
 
 MIDDLEWARE = [
@@ -68,7 +68,7 @@ if SQL:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'NAME': 'db.sqlite3',
         }
     }
 else:
