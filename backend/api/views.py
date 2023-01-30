5d60a38e017ec99fe5ac0ac2317ba340b224f0ea
Enoth <58894013+nikitairl@users.noreply.github.com>
2023-01-30 20:48:41 +0300

    test

--- backend/api/views.py
+++ backend/api/views.py
@@ -16,7 +16,11 @@ from .serializers import (CartSerializer, FavoriteSerializer,
                           IngredientSerializer, RecipeSerializer,
                           RecipeSerializerPost, RegistrationSerializer,
                           SubscriptionSerializer, TagSerializer)
-from .utils import canvas_method
+
+from reportlab.lib.pagesizes import A4
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfgen import canvas
 
 
 class CreateUserView(UserViewSet):
@@ -113,9 +117,47 @@ class FavoriteViewSet(BaseFavoriteCartViewSet):
 class DownloadCart(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
-    def download(self, request) -> HttpResponse:
+    @staticmethod
+    def canvas_method(dictionary):
+        """
+        Метод сохранения списка покупок в формате PDF.
+        """
+        response = HttpResponse(content_type='application/pdf')
+        response[
+            'Content-Disposition'] = 'attachment; \
+        filename = "shopping_cart.pdf"'
+        begin_position_x, begin_position_y = 40, 650
+        sheet = canvas.Canvas(response, pagesize=A4)
+        pdfmetrics.registerFont(TTFont('FreeSans',
+                                       'data/FreeSans.ttf'))
+        sheet.setFont('FreeSans', 50)
+        sheet.setTitle('Список покупок')
+        sheet.drawString(begin_position_x,
+                         begin_position_y + 40, 'Список покупок: ')
+        sheet.setFont('FreeSans', 24)
+        for number, item in enumerate(dictionary, start=1):
+            if begin_position_y < 100:
+                begin_position_y = 700
+                sheet.showPage()
+                sheet.setFont('FreeSans', 24)
+            sheet.drawString(
+                begin_position_x,
+                begin_position_y,
+                f'{number}.  {item["ingredient__name"]} - '
+                f'{item["ingredient_total"]}'
+                f' {item["ingredient__measurement_unit"]}'
+            )
+            begin_position_y -= 30
+        sheet.showPage()
+        sheet.save()
+        return response
+
+    def download(self, request):
+        """
+        Метод создания списка покупок.
+        """
         result = IngredientRecipe.objects.filter(
-            recipe__carts__user=request.user).values_list(
+            recipe__carts__user=request.user).values(
             'ingredient__name', 'ingredient__measurement_unit').order_by(
                 'ingredient__name').annotate(ingredient_total=Sum('amount'))
-        return canvas_method(result)
+        return self.canvas_method(result)
