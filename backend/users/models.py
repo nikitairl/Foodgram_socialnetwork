e888e64b210a7a92b8a459a2e042354bc24eb3ee
Enoth <58894013+nikitairl@users.noreply.github.com>
2023-01-30 17:54:31 +0300

    test

--- backend/users/models.py
+++ backend/users/models.py
@@ -3,36 +3,32 @@ from django.db import models
 
 
 class User(AbstractUser):
-    username: str = models.CharField(
+    username = models.CharField(
         db_index=True,
         max_length=150,
         unique=True,
-        verbose_name='Имя пользователя',
-        help_text='Введите уникальное имя пользователя',
-    )
-    email: str = models.EmailField(
+        verbose_name='Имя',
+        help_text='Имя должно быть уникальным')
+    email = models.EmailField(
         db_index=True,
-        max_length=254,
         unique=True,
+        max_length=254,
         verbose_name='Электронная почта',
-        help_text='Введите электронную почту'
-    )
-    first_name: str = models.CharField(
+        help_text='Введите электронную почту пользователя')
+    first_name = models.CharField(
         max_length=150,
         verbose_name='Имя',
-        help_text='Имя пользователя',
-    )
-    last_name: str = models.CharField(
+        help_text='Введите имя пользователя')
+    last_name = models.CharField(
         max_length=150,
         verbose_name='Фамилия',
-        help_text='Фамилия пользователя'
-    )
+        help_text='Введите фамилию пользователя')
     is_subscribed = models.BooleanField(
         default=False,
         verbose_name='Подписка на данного пользователя',
-        help_text='Отметьте для подписки на данного пользователя')
-    USERNAME_FIELD: email = 'email'
-    REQUIRED_FIELDS: str = ['username', 'first_name', 'last_name', 'password']
+        help_text='Отметьте для подписки на пользователя')
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name', 'password']
 
-    def __str__(self) -> str:
+    def __str__(self):
         return self.username
